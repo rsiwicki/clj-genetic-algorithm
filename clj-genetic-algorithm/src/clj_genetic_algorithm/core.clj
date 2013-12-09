e89369de71c9a17e4079e9206b569f2ef6d8547d
robert.siwicki@googlemail.com <rob@Robs-MacBook-Air.local>
2013-12-09 09:53:00 +0000

    add

---
+++ clj-genetic-algorithm/src/clj_genetic_algorithm/core.clj
@@ -0,0 +1,111 @@
+(ns clj-genetic-algorithm.core)
+
+(def initial-population-size 4)
+(def min-fitness 0)
+(def cross-over-probability 0.2)
+
+(defn avg-population-fitness [population] 
+  (/ (reduce + (map #(:fitness %) population)) (count population)))
+
+(defn max-population-fitness [population]
+  (reduce max (map #(:fitness %) population)))
+
+(defn rnd-int [] (int (* (rand) 10)))
+
+(def gene-pool [:DUP :SWAP :MUL :ADD :OVER :NOP])
+
+(defn get-op [] (rand-nth gene-pool))
+
+(defn build-genome []
+  (for [x (range 10)] (get-op)))
+
+(defn build-population [] 
+  (for [x (range initial-population-size)] (assoc {:fitness 0} :genome (build-genome))))
+
+; returns new stack post operator 
+; e.g. (run-vm [:SWAP :DUP :ADD :ADD :ADD :DUP :MUL :DUP :MUL] [1 2 3])
+; => [65536]
+(defn cycle-virt-mach [instruction stack]
+  (case instruction 
+    :DUP (conj stack (peek stack))
+    :SWAP (let [l (last stack) n (nth stack 1)] (conj (pop (pop stack)) l n))
+    :MUL (conj (pop (pop stack)) (* (last stack) (nth stack 1)))
+    :ADD (conj (pop (pop stack)) (+ (last stack) (nth stack 1)))
+    :OVER (conj (pop stack) (first stack))
+    :NOP stack
+    ))
+
+(defn run-virt-mach [instruction-set stack]
+  (let [[instruction & residual-instruction-set] instruction-set ]
+  	(if (nil? instruction)
+     stack
+   	(recur residual-instruction-set (cycle-virt-mach instruction stack)))))
+
+; testing x^3 + y^2 + z  
+; returns fitness
+(defn evaluate-genome [genome]
+  (try
+  	(let [x (rnd-int) 
+        y (rnd-int) 
+        z (rnd-int) 
+        answer (+ (* x x x) (* y y) z)
+        stack (conj [] x y z)
+        phenotype (run-virt-mach genome stack)]
+     (if (= (count phenotype) 1) 
+       (if (= (first phenotype) answer) 100 10) 0))
+  (catch Exception e -10) ; your genome broke the virtual machine!
+  )) 
+
+(defn splice-genome [genome]
+  (split-at (int (* (rand) (count genome))) genome)
+  )
+
+; need a probability to make it less destructive
+(defn reproduce [parent-xx parent-xy]
+  (let [p-x (splice-genome parent-xx)
+        p-y (splice-genome parent-xy)
+        new-xx (concat (first p-x) (last p-y))
+        new-xy (concat (first p-y) (last p-x))
+        ]
+    	(if (< (rand) cross-over-probability) 
+       		(list new-xx new-xy)
+         	(list parent-xx parent-xy))))
+
+; need to make fitness additive
+(defn evaluate-population [population]
+  (map #(assoc {} :genome % :fitness (evaluate-genome %)) (map #(:genome %) (build-population))))
+
+(defn evolve [population] 
+  (let [p evaluate-population
+        max-fitness (max-population-fitness p)
+        avg-fitness (avg-population-fitness p)]
+		    	
+     
+    
+    
+ p   
+ ))
+
+; selectes a parent at random based on the proability related to fitness
+; if no parent is found after probing 10% of population
+; returns random candidate
+(defn select-parent [population max-fitness min-fitness breaker] 
+  (let [candidate-genome (rand-nth population)
+        candidate-fitness (:fitness candidate-genome)
+        selection-probability (/ candidate-fitness max-fitness)
+        max-tie-breaker 100]   	
+     	(if (= breaker max-tie-breaker)
+     		(rand-nth population)
+      	(if 
+       		(and (> candidate-fitness min-fitness) 
+              	 (< (rand) selection-probability))
+       		candidate-genome
+            (recur population max-fitness min-fitness (inc breaker))
+        ))
+         
+  ))
+
+(defn recombine [] 1)
+
+
+(defn main [] 1)
---
+++ clj-genetic-algorithm/src/clj_genetic_algorithm/virtual-machine.clj
@@ -0,0 +1,3 @@
+(ns clj-genetic-algorithm.virtual-machine)
+
+(def vmf 100)
